4b320bc0abba2721166194a20517d2f51ec2d9b4
ryubsmile <jefff.ryu@gmail.com>
2026-03-05 00:55:28 -0500

    feat: add api-2

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from database import engine, Base
-from routes import users
+from routes import users, business
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(business.router)
