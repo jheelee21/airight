ced7a2724a558f5106152a373c9c55f7d8ec0c1a
ryubsmile <jefff.ryu@gmail.com>
2026-03-04 23:15:48 -0500

    feat: add basic models

---
+++ backend/app/models/node.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+
+class Node(Base):
+    """
+    Node is a physical location that is part of a business's supply chain.
+    E.g. factory, warehouse, distribution center, supplier, oem, etc.
+
+    Not necessarily owned by the business. It can be a third party.
+    """
+    __tablename__ = "node"
+
+    id = Column(Integer, primary_key=True, index=True)
+    business_id = Column(Integer, ForeignKey("business.id"), nullable=False)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+    # TODO: add more ... domain related columns
